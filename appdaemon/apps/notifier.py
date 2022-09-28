422baf3becd9bdaa489b17bf6def79fb1e8324ae
André Fortuna <tagcashdev@gmail.com>
2022-09-28 16:05:03 +0200

    Add Functions - Ajout fonctions
    Create Fork for beginner with AppDaemon
    Add send_to_all : For send for all [persons] with no conditions
    Add send_to_no_present: For send for all [persons] who are not at home
    
    Example : home_occupancy_sensor_id with group‧presence (home/not_home)
    
    -----------------------
    
    Création d'un fork pour les débutant appdaemon qui n'ose pas trop toucher le code
    ajout action "send_to_all" : Pour toutes les [persons] sans conditions
    ajout action "send_to_no_present" : Pour toutes les [persons] qui ne sont pas à la maison (sans prendre en compte la proximité)
    - Exemple de situation :
    Ma femme travail à proximité de la maison, mais ne peux pas quitter son travail sauf pendant les pauses (qui bien sûr ne sont jamais à la même heure sinon la vie est trop facile), je travail un peu plus loin mais si je peux quitter le travail quand je veux, si une averse est détecté et que la porte du balcon est ouvert, je souhaite envoyé une notification à toutes les personnes hors de la maison
    Ou si quelqu'un de la famille à une clé et viens pour x ou y raison, on peu ainsi notifier également toutes les personnes hors de la maison que la porte à été ouverte 
    Ou .... à votre convenance.
    
    Example : home_occupancy_sensor_id with group‧presence (home/not_home)

--- appdaemon/apps/notifier.py
+++ appdaemon/apps/notifier.py
@@ -8,6 +8,7 @@ Notify is an app responsible for notifying the right occupant(s) at the right ti
 Here is the list of parameter that you NEED to set in order to run the app:
 
 home_occupancy_sensor_id: the id of a binary sensor that will be true if someone is at home, and false otherwise
+TagCashDev Fork home_occupancy_sensor_id: id of group‧presence use "home" / "not_home"
 proximity_threshold: A thresold in meter, bellow this threshold the app will concider the person at home. This is to avoid pinging only the first one that reaches Home if both occupant are on the same car for exmaple (Both will be pinged)
 persons: A list of person, including
     name: their name
@@ -21,17 +22,17 @@ Here is an exmaple on how to instantiate the app:
 notifier:
   module: notifier
   class: notifier
-  home_occupancy_sensor_id: binary_sensor.home_occupied
+  home_occupancy_sensor_id: binary_sensor‧home_occupied
   proximity_threshold: 1000
   persons:
     - name: jl
-      id: person.jenova70
+      id: person‧jenova70
       notification_service: notify/mobile_app_pixel_6
-      proximity_id: proximity.distance_jl_home
+      proximity_id: proximity‧distance_jl_home
     - name: valentine
-      id: person.valentine
+      id: person‧valentine
       notification_service: notify/mobile_app_pixel_4a
-      proximity_id: proximity.distance_valentine_home
+      proximity_id: proximity‧distance_valentine_home
 
 The complete app can be called from anywhere by sending a custom event NOTIFIER with the following schema:
 
@@ -102,9 +103,9 @@ until dynamically creates watcher(s) to clear notification.
 I prefer to explain it with an example:
 If you want to notify all occupants that the lights are still on while the home is empty, you can specify
 until:
- - entity_id: binary_sensor.home_occupied
+ - entity_id: binary_sensor‧home_occupied
    new_state : on
- - entity_id: light.all_lights
+ - entity_id: light‧all_lights
    new_state : off
 This will make the notification(s) disappear as soon as the lights are off, or the home becomes occupied.
 That way, you make sure notifications are only displayed when relevant.
@@ -114,75 +115,82 @@ That way, you make sure notifications are only displayed when relevant.
 class notifier(hass.Hass): 
     def initialize(self):
         # Listen to all NOTIFIER events
-        self.listen_event(self.callback_notifier_called , "NOTIFIER")
-        self.listen_event(self.callback_button_clicked, "mobile_app_notification_action")
-        self.listen_event(self.callback_notification_cleared, "mobile_app_notification_cleared")
+        self‧listen_event(self‧callback_notifier_called , "NOTIFIER")
+        self‧listen_event(self‧callback_button_clicked, "mobile_app_notification_action")
+        self‧listen_event(self‧callback_notification_cleared, "mobile_app_notification_cleared")
         
         # Staged notification 
-        self.staged_notifications = []
-        self.listen_state(self.callback_home_occupied , self.args["home_occupancy_sensor_id"] , old = "off" , new = "on")
+        self‧staged_notifications = []
+        self‧listen_state(self‧callback_home_occupied , self‧args["home_occupancy_sensor_id"] , old = "not_home" , new = "home") 
+        # TagCashDev Fork home_occupancy_sensor_id change off -> not_home and on -> home
 
         # Temporary watchers
-        self.watchers_handles = []
+        self‧watchers_handles = []
 
         # log
-        self.log("Notifier initialized")  
+        self‧log("Notifier initialized")  
 
     def callback_notifier_called(self, event_name, data, kwargs):
-        self.log("NOTIFIER event received")  
+        self‧log("NOTIFIER event received")  
         if "action" in data:
             action = data["action"]
-            for person in self.args["persons"]:
+            if action == "send_to_all":
+                # TagcashDev Fork (send_to_all)
+                self‧send_to_all(data)
+            for person in self‧args["persons"]:
                 if action == "send_to_" + person["name"]:
-                    self.send_to_person(data, person)
+                    self‧send_to_person(data, person)
             if action == "send_to_present":
-                #send_to_present
-                self.send_to_present(data)
+                # send_to_present
+                self‧send_to_present(data)
             if action == "send_to_nearest":
-                #send_to_nearest
-                self.send_to_nearest(data)
+                # send_to_nearest
+                self‧send_to_nearest(data)
+            if action == "send_to_no_present":
+                # TagcashDev Fork (send_to_no_present)
+                self‧send_to_no_present(data) 
             if action == "send_when_present":
-                #send_when_present
-                self.send_when_present(data) 
+                # send_when_present
+                self‧send_when_present(data) 
         
         if "persistent" in data:
             if data["persistent"]:
-                self.log("Persisting the notification on Home Assistant Front-end ...")
-                self.call_service("notify/persistent_notification", title = data["title"], message = data["message"])
+                self‧log("Persisting the notification on Home Assistant Front-end ...")
+                self‧call_service("notify/persistent_notification", title = data["title"], message = data["message"])
         
         if "until" in data and 'tag' in data:
             until = data["until"]
             for watcher in until:
                 watcher_handle = {}
-                watcher_handle["id"] = self.listen_state(self.callback_until_watcher, watcher["entity_id"], new = str(watcher["new_state"]), oneshot = True, tag = data["tag"])
+                watcher_handle["id"] = self‧listen_state(self‧callback_until_watcher, watcher["entity_id"], new = str(watcher["new_state"]), oneshot = True, tag = data["tag"])
                 watcher_handle["tag"] = data["tag"]
-                self.watchers_handles.append(watcher_handle)
-                self.log("All notifications with tag " + data["tag"] + " will be cleared if " + watcher["entity_id"] + " transitions to " + str(watcher["new_state"]))
+                self‧watchers_handles‧append(watcher_handle)
+                self‧log("All notifications with tag " + data["tag"] + " will be cleared if " + watcher["entity_id"] + " transitions to " + str(watcher["new_state"]))
 
     def callback_until_watcher(self, entity, attribute, old, new, kwargs):
-        self.clear_notifications(kwargs["tag"])
+        self‧clear_notifications(kwargs["tag"])
 
     def callback_button_clicked(self, event_name, data, kwargs):
         if "tag" in data:
-            self.clear_notifications(data["tag"])
+            self‧clear_notifications(data["tag"])
 
     def callback_notification_cleared(self, event_name, data, kwargs):
         if "tag" in data:
-            self.clear_notifications(data["tag"])
+            self‧clear_notifications(data["tag"])
     
     def clear_notifications(self, tag):
-        self.log("Clearing notifications with tag " + tag + " (if any) ...")
+        self‧log("Clearing notifications with tag " + tag + " (if any) ...")
         notification_data = {}
         notification_data["tag"] = tag
-        for person in self.args["persons"]:
-            self.call_service(person["notification_service"], message = "clear_notification", data = notification_data)
-        self.cancel_watchers(tag)
+        for person in self‧args["persons"]:
+            self‧call_service(person["notification_service"], message = "clear_notification", data = notification_data)
+        self‧cancel_watchers(tag)
 
     def cancel_watchers(self, tag):
-        self.log("Removing watchers with tag " + tag + " (if any) ...")
-        for watcher in list(self.watchers_handles):
+        self‧log("Removing watchers with tag " + tag + " (if any) ...")
+        for watcher in list(self‧watchers_handles):
             if watcher["tag"] == tag:
-                self.watchers_handles.remove(watcher)
+                self‧watchers_handles‧remove(watcher)
         
     def build_notification_data(self, data):
         notification_data = {}
@@ -193,7 +201,7 @@ class notifier(hass.Hass):
                     "action":callback["event"],
                     "title":callback["title"]
                 }
-                notification_data["actions"].append(action)
+                notification_data["actions"]‧append(action)
         if "timeout" in data:
             notification_data["timeout"] = data["timeout"]
         if "click_url" in data:
@@ -208,42 +216,61 @@ class notifier(hass.Hass):
             notification_data["tag"] = data["tag"]
         return notification_data
 
+    # TagcashDev Fork (send_to_all): Add function for send for all [persons] with no conditions
+    def send_to_all(self, data):
+        number_of_notification_sent = 0
+        for person in self‧args["persons"]:
+            self‧send_to_person(data, person)
+            number_of_notification_sent += 1
+
     def send_to_person(self, data, person):
-        self.log("Sending notification to " + person["name"])
-        notification_data = self.build_notification_data(data)
-        self.call_service(person["notification_service"], title = data["title"], message = data["message"], data = notification_data)
+        self‧log("Sending notification to " + person["name"])
+        notification_data = self‧build_notification_data(data)
+        self‧call_service(person["notification_service"], title = data["title"], message = data["message"], data = notification_data)
     
     def send_to_present(self, data):
         number_of_notification_sent = 0
-        for person in self.args["persons"]:
-            if self.get_state(person["id"]) == "home" or float(self.get_state(person["proximity_id"])) <= self.args["proximity_threshold"]:
-                self.send_to_person(data, person)
+        for person in self‧args["persons"]:
+            if self‧get_state(person["id"]) == "home" or float(self‧get_state(person["proximity_id"])) <= self‧args["proximity_threshold"]:
+                self‧send_to_person(data, person)
                 number_of_notification_sent += 1
         if number_of_notification_sent == 0:
-            #defaulting to nearest
-            self.send_to_nearest(data)
+            # defaulting to nearest
+            self‧send_to_nearest(data)
 
     def send_to_nearest(self, data):
-        min_proximity = float(self.get_state(self.args["persons"][0]["proximity_id"]))
-        for person in self.args["persons"]:
-            person_proximity = float(self.get_state(person["proximity_id"]))
+        min_proximity = float(self‧get_state(self‧args["persons"][0]["proximity_id"]))
+        for person in self‧args["persons"]:
+            person_proximity = float(self‧get_state(person["proximity_id"]))
             if person_proximity <= min_proximity:
                 min_proximity = person_proximity
 
-        for person in self.args["persons"]:
-            person_proximity = float(self.get_state(person["proximity_id"]))
-            if person_proximity <= min_proximity + self.args["proximity_threshold"]:
-                self.send_to_person(data, person)
+        for person in self‧args["persons"]:
+            person_proximity = float(self‧get_state(person["proximity_id"]))
+            if person_proximity <= min_proximity + self‧args["proximity_threshold"]:
+                self‧send_to_person(data, person)
+
+    # TagcashDev Fork: Add function for send for all [persons] who are not at home
+    def send_to_no_present(self, data):
+        number_of_notification_sent = 0
+        for person in self‧args["persons"]:
+            if self‧get_state(person["id"]) != "home":
+                self‧send_to_person(data, person)
+                number_of_notification_sent += 1
+        if number_of_notification_sent == 0:
+            # defaulting to when present, in case have bug ...
+            self‧send_when_present(data)
     
     def send_when_present(self, data):
-        if self.get_state(self.args["home_occupancy_sensor_id"]) == "on":
-            self.send_to_present(data)
+        # TagCashDev Fork home_occupancy_sensor_id change on -> home
+        if self‧get_state(self‧args["home_occupancy_sensor_id"]) == "home":
+            self‧send_to_present(data)
         else:
-            self.log("Staging notification for when home becomes occupied ...")
-            self.staged_notifications.append(data)
+            self‧log("Staging notification for when home becomes occupied ...")
+            self‧staged_notifications‧append(data)
     
     def callback_home_occupied(self, entity, attribute, old, new, kwargs):
-        self.log("Home is occupied ... Checking if we need to send some notification now ...")
-        while len(self.staged_notifications) >= 1:
-            current_data = self.staged_notifications.pop(0)
-            self.send_to_present(current_data)
+        self‧log("Home is occupied ... Checking if we need to send some notification now ...")
+        while len(self‧staged_notifications) >= 1:
+            current_data = self‧staged_notifications‧pop(0)
+            self‧send_to_present(current_data)
